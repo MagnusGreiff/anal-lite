ea3778b90e50dc5fc1bdba3984fc842f37f7f145
root <mangegreiff@gmail.com>
2017-04-04 12:28:09 +0200

    Updated navbar

--- config/route/base.php
+++ config/route/base.php
@@ -9,7 +9,7 @@
 
 $app->router->add("", function () use ($app) {
     $app->view->add("take1/header", ["title" => "Home"]);
-    $app->view->add("take1/navbar");
+    $app->view->add("take1/navbar1");
     $app->view->add("take1/home");
     $app->view->add("take1/footer");
 
@@ -18,7 +18,7 @@ $app->router->add("", function () use ($app) {
 
 $app->router->add("about", function () use ($app) {
     $app->view->add("take1/header", ["title" => "About"]);
-    $app->view->add("take1/navbar");
+    $app->view->add("take1/navbar1");
     $app->view->add("take1/about");
     $app->view->add("take1/footer");
 
@@ -27,7 +27,7 @@ $app->router->add("about", function () use ($app) {
 
 $app->router->add("report", function () use ($app) {
     $app->view->add("take1/header", ["title" => "Report"]);
-    $app->view->add("take1/navbar");
+    $app->view->add("take1/navbar1");
     $app->view->add("take1/report");
     $app->view->add("take1/footer");
 
@@ -36,7 +36,7 @@ $app->router->add("report", function () use ($app) {
 
 $app->router->add("status", function () use ($app) {
     $app->view->add("take1/header", ["title" => "Status"]);
-    $app->view->add("take1/navbar");
+    $app->view->add("take1/navbar1");
     $app->view->add("take1/status");
     $app->view->add("take1/footer");
 
@@ -45,7 +45,7 @@ $app->router->add("status", function () use ($app) {
 
 $app->router->add("details", function () use ($app) {
     $app->view->add("take1/header", ["title" => "Details"]);
-    $app->view->add("take1/navbar");
+    $app->view->add("take1/navbar1");
     $app->view->add("take1/details");
     $app->view->add("take1/footer");
 
--- view/take1/about.php
+++ view/take1/about.php
@@ -1,10 +1,12 @@
 <?php
- $details = $app->url->create("details")
+$details = $app->url->create("details");
+$numberGame = $app->url->create("../../../kmom01/guess/index.php");
 ?>
 <h1>Om Kursen</h1>
 <div class="text_content">
  <p class="text">Denna webbsidan är en del av kursen <a href="http://www.dbwebb.se/kurser/oophp">oophp</a> som lärs ut på Blekinge Tekniska Högskola.</p>
  <p class="text">Länk till anax-lite på github: <a href="https://github.com/MagnusGreiff/anal-lite">Anax-lite</a></p>
  <p class="text">Information: <a href="<?= $details ?>">Klicka Här</a></p>
+ <p class="text">Guess the number game:  <a href="<?= $numberGame ?>">Klicka Här</a></p>
 </div>
 <img class="image" src="../../htdocs/img/php_image.png" alt="Bild på PHP">
